e874089a8e7a762a4e2ac534d03edb4cf1ee8c7e
YusifE <yusifelawawdeh@gmail.com>
2018-05-24 22:42:58 -0500

    made post method async, plus allows usage of users in api, brain may be mush at the moment wait till next commit mitt kafe bitte

--- DutchTreats/Controllers/OrdersController.cs
+++ DutchTreats/Controllers/OrdersController.cs
@@ -4,6 +4,7 @@ using DutchTreats.Data.Entities;
 using DutchTreats.ViewModels;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -15,18 +16,20 @@ using System.Threading.Tasks;
 namespace DutchTreats.Controllers
 {
     [Route("api/[Controller]")]
-    [Authorize(AuthenticationSchemes=JwtBearerDefaults.AuthenticationScheme)]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class OrdersController : Controller
     {
         private readonly IDutchRepository _repository;
         private readonly ILogger<OrdersController> _logger;
         private readonly IMapper _mapper;
+        private readonly UserManager<StoreUser> _userManager;
 
-        public OrdersController(IDutchRepository repository, ILogger<OrdersController> logger, IMapper mapper)
+        public OrdersController(IDutchRepository repository, ILogger<OrdersController> logger, IMapper mapper, UserManager<StoreUser> userManager)
         {
             _repository = repository;
             _logger = logger;
             _mapper = mapper;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -70,7 +73,7 @@ namespace DutchTreats.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody]OrderViewModel model)
+        public async Task<IActionResult> Post([FromBody]OrderViewModel model)
         {
             try
             {
@@ -82,6 +85,9 @@ namespace DutchTreats.Controllers
                     {
                         newOrder.OrderDate = DateTime.Now;
                     }
+
+                    var currentUser = await _userManager.FindByNameAsync(User.Identity.Name);
+                    newOrder.User = currentUser;
 
                     _repository.AddEntity(newOrder);
                     if (_repository.SaveAll())
